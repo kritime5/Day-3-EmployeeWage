4abe00253284e7aaace6c8e07245564baacedb0f
Kritika <kritsme963@gmail.com>
2022-10-18 05:31:22 -0700

    Added:Maxmimum condition

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,19 +1,28 @@
 package com.bridgelabz;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.sql.SQLOutput;
 
 public class EmpWage {
-   public static final  int IS_PART_TIME = 1;
-   public static final int IS_FULL_TIME = 2;
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
+    public static final int NUMBER_OF_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation");
-        //UC4-using switch case
+        //UC6-CALCULATE WAGE TILL MAX CONDITION
         //VARIABLES
         int empHrs = 0;
         int empWage = 0;
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
         //computation
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS)
+            totalWorkingDays++;
         int a = (int) (Math.random() * 3);
         switch (a) {
             case IS_PART_TIME:
@@ -27,9 +36,7 @@ public class EmpWage {
         }
 
         empWage = empHrs * EMP_WAGE_PER_HOUR;
-        System.out.println("Emp Wage:" + empWage);
-
+        System.out.println("TOTAL WORKING DAYS=" + totalWorkingDays + "Total employee hours=" + empHrs);
 
     }
 }
-
