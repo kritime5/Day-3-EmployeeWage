3cae30df204b60cf548697cf6bb27819caad8202
Kritika <kritsme963@gmail.com>
2022-10-18 04:53:03 -0700

    Added:Switch case

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -3,30 +3,33 @@ package com.bridgelabz;
 import java.sql.SQLOutput;
 
 public class EmpWage {
+   public static final  int IS_PART_TIME = 1;
+   public static final int IS_FULL_TIME = 2;
+    public static final int EMP_WAGE_PER_HOUR = 20;
+
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation");
-        //UC3-ADD PART TIME EMPLOYEE AND WAGE-PART TIME HR IS 8
-        //CONSTANTS
-        int IS_PART_TIME=1;
-        int IS_FULL_TIME=2;
-        int EMP_WAGE_PER_HOUR=20;
-
+        //UC4-using switch case
         //VARIABLES
-        int empHrs=0;
-        int empWage=0;
+        int empHrs = 0;
+        int empWage = 0;
         //computation
-        int a=(int)(Math.random()*2);
-        if(a==IS_PART_TIME)
-            empHrs=4;
-        else if (a==IS_FULL_TIME)
-            empHrs=8;
-        else
-            empHrs=0;
+        int a = (int) (Math.random() * 3);
+        switch (a) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
 
-        empWage=empHrs*EMP_WAGE_PER_HOUR;
-        System.out.println("Emp Wage:"+empWage);
-
+        empWage = empHrs * EMP_WAGE_PER_HOUR;
+        System.out.println("Emp Wage:" + empWage);
 
 
     }
 }
+
