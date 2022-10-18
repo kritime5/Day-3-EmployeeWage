69317479c769fccc062fb8ae395446fa032f1ab9
Kritika <kritsme963@gmail.com>
2022-10-18 03:51:02 -0700

    Added:Whether employee is present or absent

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation");
+        int a=(int)(Math.random()*2);
+        if(a==1) {
+            System.out.println("The employee is present");
+        }else{
+            System.out.println("Employee is present");
+        }
     }
 }
